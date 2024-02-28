f2014e702021472c18faf1bb6e03945128dfb7cb
AdamBalski <adambalski1@gmail.com>
2024-02-28 17:26:44 +0100

    ADD: iCal grammar parsing for one line at a time

--- src/__tests__/util/prolog/icsParsing.ts
+++ src/__tests__/util/prolog/icsParsing.ts
@@ -108,10 +108,10 @@ describe("unfold/2", () => {
     test("unfold/2 2", expectOutputToBe(
         session,
         icsParsingPro,
-        'unfold(["a", "xd"]).',
+        'unfold(["a", "xd"], R).',
         ["R = [[a],[x,d]]"]
     ));
-    test("unfold/2 2", expectOutputToBe(
+    test("unfold/2 3", expectOutputToBe(
         session,
         icsParsingPro,
         'unfold([], R).',
@@ -138,4 +138,361 @@ describe("fileToLines/2", () => {
         'fileToLines("\n\naa", R).',
         ["R = [[a,a]]"]
     ));
+});
+
+// '\t', '👍', '\r', '\n', ' ', 'a', '!', '\', '\v', ';', ':', ',', '\"', 'ą', '漢', '𓀀'
+describe("value_char//1", () => {
+    test("value_char \\t", expectOutputToBe(
+        session, icsParsingPro, "phrase(value_char(X), ['\\t']).", ["X = \t"]
+    ));
+
+    test("value_char 👍", expectOutputToBe(
+        session, icsParsingPro, "phrase(value_char(X), ['👍']).", ["X = 👍"]
+    ));
+
+    test("value_char \\r", expectOutputToBe(
+        session, icsParsingPro, "phrase(value_char(X), ['\\r']).", []
+    ));
+
+    test("value_char \\n", expectOutputToBe(
+        session, icsParsingPro, "phrase(value_char(X), ['\\n']).", []
+    ));
+
+    test("value_char space", expectOutputToBe(
+        session, icsParsingPro, "phrase(value_char(X), [' ']).", ["X =  "]
+    ));
+
+    test("value_char a", expectOutputToBe(
+        session, icsParsingPro, "phrase(value_char(X), ['a']).", ["X = a"]
+    ));
+
+    test("value_char !", expectOutputToBe(
+        session, icsParsingPro, "phrase(value_char(X), ['!']).", ["X = !"]
+    ));
+
+    test("value_char \\", expectOutputToBe(
+        session, icsParsingPro, "phrase(value_char(X), ['\\\\']).", ["X = (\\)"]
+    ));
+
+    test("value_char \\v", expectOutputToBe(
+        session, icsParsingPro, "phrase(value_char(X), ['\\v']).", []
+    ));
+
+    test("value_char DEL", expectOutputToBe(
+        session, icsParsingPro, "phrase(value_char(X), ['\u007F']).", []
+    ));
+
+    test("value_char ;", expectOutputToBe(
+        session, icsParsingPro, "phrase(value_char(X), [';']).", ["X = ;"]
+    ));
+
+    test("value_char :", expectOutputToBe(
+        session, icsParsingPro, "phrase(value_char(X), [':']).", ["X = (:)"]
+    ));
+
+    test("value_char ,", expectOutputToBe(
+        session, icsParsingPro, "phrase(value_char(X), [',']).", ["X = ,"]
+    ));
+
+    test("value_char \"", expectOutputToBe(
+        session, icsParsingPro, "phrase(value_char(X), ['\"']).", ["X = \""]
+    ));
+
+    test("value_char ą", expectOutputToBe(
+        session, icsParsingPro, "phrase(value_char(X), ['ą']).", ["X = ą"]
+    ));
+
+    test("value_char 漢", expectOutputToBe(
+        session, icsParsingPro, "phrase(value_char(X), ['漢']).", ["X = 漢"]
+    ));
+
+    test("value_char 𓀀", expectOutputToBe(
+        session, icsParsingPro, "phrase(value_char(X), ['𓀀']).", ["X = 𓀀"]
+    ));
+});
+describe("qsafe_char//1", () => {
+    test("qsafe_char \\t", expectOutputToBe(
+        session, icsParsingPro, "phrase(qsafe_char(X), ['\\t']).", ["X = \t"]
+    ));
+
+    test("qsafe_char 👍", expectOutputToBe(
+        session, icsParsingPro, "phrase(qsafe_char(X), ['👍']).", ["X = 👍"]
+    ));
+
+    test("qsafe_char \\r", expectOutputToBe(
+        session, icsParsingPro, "phrase(qsafe_char(X), ['\\r']).", []
+    ));
+
+    test("qsafe_char \\n", expectOutputToBe(
+        session, icsParsingPro, "phrase(qsafe_char(X), ['\\n']).", []
+    ));
+
+    test("qsafe_char space", expectOutputToBe(
+        session, icsParsingPro, "phrase(qsafe_char(X), [' ']).", ["X =  "]
+    ));
+
+    test("qsafe_char a", expectOutputToBe(
+        session, icsParsingPro, "phrase(qsafe_char(X), ['a']).", ["X = a"]
+    ));
+
+    test("qsafe_char !", expectOutputToBe(
+        session, icsParsingPro, "phrase(qsafe_char(X), ['!']).", ["X = !"]
+    ));
+
+    test("qsafe_char \\", expectOutputToBe(
+        session, icsParsingPro, "phrase(qsafe_char(X), ['\\\\']).", ["X = (\\)"]
+    ));
+
+    test("qsafe_char \\v", expectOutputToBe(
+        session, icsParsingPro, "phrase(qsafe_char(X), ['\\v']).", []
+    ));
+
+    test("qsafe_char DEL", expectOutputToBe(
+        session, icsParsingPro, "phrase(qsafe_char(X), ['\u007F']).", []
+    ));
+
+    test("qsafe_char ;", expectOutputToBe(
+        session, icsParsingPro, "phrase(qsafe_char(X), [';']).", ["X = ;"]
+    ));
+
+    test("qsafe_char :", expectOutputToBe(
+        session, icsParsingPro, "phrase(qsafe_char(X), [':']).", ["X = (:)"]
+    ));
+
+    test("qsafe_char ,", expectOutputToBe(
+        session, icsParsingPro, "phrase(qsafe_char(X), [',']).", ["X = ,"]
+    ));
+
+    test("qsafe_char \"", expectOutputToBe(
+        session, icsParsingPro, "phrase(qsafe_char(X), ['\"']).", []
+    ));
+
+    test("qsafe_char ą", expectOutputToBe(
+        session, icsParsingPro, "phrase(qsafe_char(X), ['ą']).", ["X = ą"]
+    ));
+
+    test("qsafe_char 漢", expectOutputToBe(
+        session, icsParsingPro, "phrase(qsafe_char(X), ['漢']).", ["X = 漢"]
+    ));
+
+    test("qsafe_char 𓀀", expectOutputToBe(
+        session, icsParsingPro, "phrase(qsafe_char(X), ['𓀀']).", ["X = 𓀀"]
+    ));
+});
+
+describe("safe_char//1", () => {
+    test("safe_char \\t", expectOutputToBe(
+        session, icsParsingPro, "phrase(safe_char(X), ['\\t']).", ["X = \t"]
+    ));
+
+    test("safe_char 👍", expectOutputToBe(
+        session, icsParsingPro, "phrase(safe_char(X), ['👍']).", ["X = 👍"]
+    ));
+
+    test("safe_char \\r", expectOutputToBe(
+        session, icsParsingPro, "phrase(safe_char(X), ['\\r']).", []
+    ));
+
+    test("safe_char \\n", expectOutputToBe(
+        session, icsParsingPro, "phrase(safe_char(X), ['\\n']).", []
+    ));
+
+    test("safe_char space", expectOutputToBe(
+        session, icsParsingPro, "phrase(safe_char(X), [' ']).", ["X =  "]
+    ));
+
+    test("safe_char a", expectOutputToBe(
+        session, icsParsingPro, "phrase(safe_char(X), ['a']).", ["X = a"]
+    ));
+
+    test("safe_char !", expectOutputToBe(
+        session, icsParsingPro, "phrase(safe_char(X), ['!']).", ["X = !"]
+    ));
+
+    test("safe_char \\", expectOutputToBe(
+        session, icsParsingPro, "phrase(safe_char(X), ['\\\\']).", ["X = (\\)"]
+    ));
+
+    test("safe_char \\v", expectOutputToBe(
+        session, icsParsingPro, "phrase(safe_char(X), ['\\v']).", []
+    ));
+
+    test("safe_char DEL", expectOutputToBe(
+        session, icsParsingPro, "phrase(safe_char(X), ['\u007F']).", []
+    ));
+
+    test("safe_char ;", expectOutputToBe(
+        session, icsParsingPro, "phrase(safe_char(X), [';']).", []
+    ));
+
+    test("safe_char :", expectOutputToBe(
+        session, icsParsingPro, "phrase(safe_char(X), [':']).", []
+    ));
+
+    test("safe_char ,", expectOutputToBe(
+        session, icsParsingPro, "phrase(safe_char(X), [',']).", []
+    ));
+
+    test("safe_char \"", expectOutputToBe(
+        session, icsParsingPro, "phrase(safe_char(X), ['\"']).", []
+    ));
+
+    test("safe_char ą", expectOutputToBe(
+        session, icsParsingPro, "phrase(safe_char(X), ['ą']).", ["X = ą"]
+    ));
+
+    test("safe_char 漢", expectOutputToBe(
+        session, icsParsingPro, "phrase(safe_char(X), ['漢']).", ["X = 漢"]
+    ));
+
+    test("safe_char 𓀀", expectOutputToBe(
+        session, icsParsingPro, "phrase(safe_char(X), ['𓀀']).", ["X = 𓀀"]
+    ));
+});
+
+describe("value//1", () => {
+   test("value 1", expectOutputToBe(
+      session, icsParsingPro, `phrase(value(X), ['\\"'|"xd\\\\2137-_!e2;,."]).`, ['X = [\",x,d,\\,2,1,3,7,-,_,!,e,2,;,,,.]']
+   ));
+
+    test("value 2", expectOutputToBe(session,
+        icsParsingPro,
+        `phrase(value(X), "").`,
+        ["X = []"]));
+
+    test("value false", expectToBeFalse(
+        session, icsParsingPro, `phrase(value(X), ['\v']).`
+    ));
+});
+
+describe("param_value//1", () => {
+
+    test("param_value 1", expectOutputToBe(
+        session,
+        icsParsingPro,
+        String.raw`phrase(param_value(X), "\u0022xd\\2137-_!e2;\u0022").`,
+        ["X = [x,d,\\,2,1,3,7,-,_,!,e,2,;]"]
+    ));
+
+    test("param_value 2", expectOutputToBe(session,
+        icsParsingPro,
+        `phrase(param_value(X), "xd\\\\2137-_!e2.").`,
+        ["X = [x,d,\\,2,1,3,7,-,_,!,e,2,.]"]));
+
+    test("param_value 3", expectOutputToBe(session,
+        icsParsingPro,
+        `phrase(param_value(X), "").`,
+        ["X = []"]
+    ));
+
+    test("param_value 4", expectOutputToBe(session,
+        icsParsingPro,
+        `phrase(param_value(X), ['\u0022', '\u0022']).`,
+        ["X = []"]
+    ));
+
+    test("param_value false 1", expectOutputToBe(
+        session, icsParsingPro, `phrase(param_value(X), "\\u2022xd\\\\2137-_!e2;,.").`, []
+    ));
+
+    test("param_value false 2", expectToBeFalse(
+        session, icsParsingPro, `phrase(param_value(X), ['\v']).`
+    ));
+
+    test("param_value false 3", expectOutputToBe(
+        session, icsParsingPro, `phrase(param_value(X), "xd\\\\2137-_!e2;").`, []
+    ));
+
+    test("param_value false 4", expectOutputToBe(
+        session,
+        icsParsingPro,
+        `phrase(param_value(X), "\\u0022xd\\\\2137\\u0022-_!e2;\\u0022").`,
+        []
+    ));
+});
+
+
+describe("name//1", () => {
+    test("name 1", expectOutputToBe(
+        session, icsParsingPro, `phrase(name(X), "abce").`, ["X = [a,b,c,e]"]
+   ));
+
+   test("name 2", expectOutputToBe(
+       session, icsParsingPro, `phrase(name(X), "ef-u2AJ").`, ["X = [e,f,-,u,2,A,J]"]
+   ));
+
+    test("name false 1", expectOutputToBe(
+        session, icsParsingPro, `phrase(name(X), "ef -u2AJ").`, []
+    ));
+
+    test("name false 2", expectOutputToBe(
+        session, icsParsingPro, `phrase(name(X), "\\\\ef-u2AJ").`, []
+    ));
+
+    test("name false 3", expectOutputToBe(
+        session, icsParsingPro, `phrase(name(X), "").`, []
+    ));
+});
+
+describe("param_values//1", () => {
+    test("param_values 1", expectOutputToBe(
+        session, icsParsingPro, `phrase(param_values(X), "aee").`, ["X = [[a,e,e]]"]
+    ));
+
+    test("param_values 2", expectOutputToBe(
+        session, icsParsingPro, `phrase(param_values(X), "aee,bce").`, ["X = [[a,e,e],[b,c,e]]"]
+    ));
+});
+
+describe("params//1", () => {
+    test("params 1", expectOutputToBe(
+        session,
+        icsParsingPro,
+        `phrase(params(X), ";x=lol;x=xd;x=;l=,").`,
+        ["X = [param([x],[[l,o,l]]),param([x],[[x,d]]),param([x],[[]]),param([l],[[],[]])]"]
+    ));
+
+    test("params 2", expectOutputToBe(
+        session, icsParsingPro, `phrase(params(X), "x=lol;x=xd;x=;l:").`, []
+    ));
+
+    test("params 3", expectOutputToBe(
+        session,
+        icsParsingPro,
+        String.raw`phrase(params(X), [';',x,'=','\u0022','\u0022']).`,
+        ["X = [param([x],[[]])]"]
+    ));
+
+    test("params 4", expectOutputToBe(
+        session, icsParsingPro, `phrase(params(X), "").`, ["X = []"]
+    ));
+
+    test("params 5", expectOutputToBe(
+        session, icsParsingPro, `phrase(params(X), ";a=x,y,z").`, ["X = [param([a],[[x],[y],[z]])]"]
+    ));
+
+    test("params 6", expectOutputToBe(
+        session, icsParsingPro, `phrase(params(X), ";a=,,").`, ["X = [param([a],[[],[],[]])]"]
+    ));
+
+    test("params 7", expectOutputToBe(
+        session, icsParsingPro, `phrase(params(X), ";a=x;").`, []
+    ));
+});
+
+describe("content_line//3", () => {
+    test("content_line 1", expectOutputToBe(
+        session,
+        icsParsingPro,
+        String.raw`phrase(content_line(Name, Params, Value), "BEGIN:VCALENDAR").`,
+        ["Name = [B,E,G,I,N], Params = [], Value = [V,C,A,L,E,N,D,A,R]"]
+    ));
+
+    test("content_line 1", expectOutputToBe(
+        session,
+        icsParsingPro,
+        String.raw`phrase(content_line(Name, Params, Value), "ORGANIZER;CN=John Doe:MAILTO:john.doe@example.com").`,
+        ["Name = [O,R,G,A,N,I,Z,E,R], Params = [param([C,N],[[J,o,h,n, ,D,o,e]])], Value = [M,A,I,L,T,O,:,j,o,h,n,.,d,o,e,@,e,x,a,m,p,l,e,.,c,o,m]"]
+    ));
+
 });
--- src/app/util/prolog/icsParsing.ts
+++ src/app/util/prolog/icsParsing.ts
@@ -27,6 +27,10 @@ unfold([A], [A]).
 unfold([A, [' '|BTail]|T], R) :- 
     append(A, BTail, ABTail),
     unfold([ABTail|T], R).
+unfold([A, [BHead|BTail]|T], [A|PartialResult]) :-
+    BHead \= ' ',
+    unfold([[BHead|BTail]|T], PartialResult).
+    
     
 split(File, [File]) :-
     doesntContainNewLineChar(File).
@@ -45,5 +49,35 @@ fileToLines(File, Lines) :-
     split(File, Split),
     omitEmptyArrays(Split, NonEmptyArrays),
     unfold(NonEmptyArrays, Lines).
+
+% iCal grammar
+content_line(N, Params, V) --> name(N), params(Params), value_char(':'), value(V).
+
+% params
+params([]) --> [].
+params([A|T]) --> param(A), params(T).
+param(param(N, V)) --> value_char(';'), name(N), value_char('='), param_values(V).
+param_values([V]) --> param_value(V).
+param_values([V|T]) --> param_value(V), value_char(','), param_values(T).
+
+% no distinction between iana-tokens and x-names for now
+alphanumdash(A) --> [A], { member(A, "ABCDEFGHIJKLMNOPQRSTUVWXYZ-abcdefghijklmnopqrstuvwxyz0123456789") }.
+name([A]) --> alphanumdash(A).
+name([A|T]) --> alphanumdash(A), name(T).
+
+param_value(A) --> paramtext(A) ; quoted(A).
+paramtext([]) --> [].
+paramtext([A|T]) --> safe_char(A), paramtext(T).
+% non empty quoted string
+quoted(A) --> value(['"']), qsafe_string(A), value(['"']).
+qsafe_string([]) --> [].
+qsafe_string([A|T]) --> qsafe_char(A), qsafe_string(T).
+
+value([]) --> [].
+value([A|T]) --> value_char(A), value(T).
+
+value_char(Char) --> [Char], { char_code(Char, Code), (Code \= 127, (Code = 9 ; Code > 31)) }.
+qsafe_char(Char) --> [Char], { phrase(value_char(Char), [Char]), Char \= '\"' }.
+safe_char(Char) --> [Char], { phrase(qsafe_char(Char), [Char]), Char \= ':', Char \= ';', Char \= ',' }.
 `;
 export {icsParsingPro};
